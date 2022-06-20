96407b751f0138eb075907143b222512d19c7cec
Agilan <agilan817@gmail.com>
2022-06-20 12:36:11 +0530

    Heroku

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ app.get('*', (req, res) => {
     }
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT ? process.env.PORT : 5000, () => {
     console.log("Server has started on port 5000");
 })
